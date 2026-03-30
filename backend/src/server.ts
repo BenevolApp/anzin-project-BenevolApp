b1ade8995c486039704ee7670523418d3953b340
Ton Nom <christ.matsanganzoulou@gmail.com>
2026-03-30 18:55:29 +0200

    add config file

---
+++ backend/src/server.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import helmet from "helmet";
+import cors from "cors";
+import dotenv from "dotenv";
+
+
+// Cofig
+const PORT = process.env.PORT || 3000;
+const app: Express = express();
+
+dotenv.config();
+
+
+
+// middleware
+app.use(helmet());
+app.use(cors());
+
+app.use(express.json());
